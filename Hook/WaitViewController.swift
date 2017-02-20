57d7ae12bae69a119df3544346e75b66a860e6f7
Pansit Wattana <pansitwattana@pansits-mac.local>
2017-02-19 22:01:01 -0800

    add animation to wait view

--- Hook/WaitViewController.swift
+++ Hook/WaitViewController.swift
@@ -10,15 +10,32 @@ import UIKit
 
 class WaitViewController: UIViewController {
 
+    @IBOutlet weak var hookWaitImage: UIImageView!
+    
+    var timer = Timer()
+    
+    var hookImageNameSet = [#imageLiteral(resourceName: "hook_sleep_mid"), #imageLiteral(resourceName: "hook_sleep_right"), #imageLiteral(resourceName: "hook_sleep_mid"), #imageLiteral(resourceName: "hook_sleep_left")]
+    
+    var index = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(WaitViewController.animate), userInfo: nil, repeats: true)
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func animate() {
+        index += 1
+        if index >= hookImageNameSet.count{
+            index = 0
+        }
+        hookWaitImage.image = hookImageNameSet[index]
     }
     
 
